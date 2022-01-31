ff2ae99b8872dae54555c122c8cca51e2219aafd
TonyTandAndroid <tonythompsoncmu@gmail.com>
2022-01-30 16:11:53 -0800

    Bump up api 31

--- app/src/main/java/ahmedadelismail/app/MainActivity.java
+++ app/src/main/java/ahmedadelismail/app/MainActivity.java
@@ -1,7 +1,7 @@
 package ahmedadelismail.app;
 
-import android.support.annotation.NonNull;
-import android.support.v7.app.AppCompatActivity;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
 import android.widget.Toast;
